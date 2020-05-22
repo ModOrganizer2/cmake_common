b5e1e02d11f838222335ab4fdf848df7f6c13289
Mikaël Capelle <capelle.mikael@gmail.com>
2020-05-22 21:00:41 +0200

    Use the same set_project_to_run function for exe, dll and plugin projects.

--- functions.cmake
+++ functions.cmake
@@ -1,12 +1,7 @@
 cmake_minimum_required(VERSION 3.16)
 
-function(set_project_to_run_from_install)
+function(set_project_to_run_from_install exec_name)
 	set(vcxproj_user_file "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.vcxproj.user")
-
-	get_target_property(output_name ${PROJECT_NAME} OUTPUT_NAME)
-	if("${output_name}" STREQUAL "output_name-NOTFOUND")
-		set(output_name ${PROJECT_NAME})
-	endif()
 
 	if(NOT EXISTS ${vcxproj_user_file})
 		file(WRITE ${vcxproj_user_file}
@@ -15,7 +10,7 @@ function(set_project_to_run_from_install)
 				  <PropertyGroup>
 					<LocalDebuggerWorkingDirectory>${CMAKE_INSTALL_PREFIX}/bin</LocalDebuggerWorkingDirectory>
 					<DebuggerFlavor>WindowsLocalDebugger</DebuggerFlavor>
-					<LocalDebuggerCommand>${CMAKE_INSTALL_PREFIX}/bin/ModOrganizer.exe</LocalDebuggerCommand>
+					<LocalDebuggerCommand>${CMAKE_INSTALL_PREFIX}/bin/${exec_name}.exe</LocalDebuggerCommand>
 				  </PropertyGroup>
 				</Project>")
 	endif()
